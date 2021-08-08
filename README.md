0e212ae7577727b672e31252e3bb020fe2ee657b
Yeison Stiven Builes Uribe <54378260+BuilesYeison@users.noreply.github.com>
2021-08-08 18:49:58 -0500

    All the interface for department coleection are already created

--- README.md
+++ README.md
@@ -1,27 +1,15 @@
 # UiEmployeeapp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.0.
+This project is the fronted for the backend project created in django
 
-## Development server
+## Steps:
+1. Create the angular project with routes
+2. Add bootstrap
+3. Create the components, navigation  and routes to these new components
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+### To connect with backend
+1. Got o environment.ts
+2. Add the follow props into environment constant list:
+  * `API-URL: "http://127.0.0.1:8000/"` put the link to backend server
+  * `PHOTO_URL:"http://127.0.0.1:8000/Photos/"` put the link where the media is saved
+3. Import http modules and consume rest api in components
--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  departments:any = [];
+  departmentName:string = "";
+  departmentId:number = 0;
+  modalTitle:string = "";
 
   ngOnInit(): void {
+    this.refreshList();
+  }
+  refreshList(){
+    this.http.get<any>(environment.API_URL+'department')
+    .subscribe(data=>{
+      this.departments=data;
+    });
+  }
+
+  addClick(){
+    this.modalTitle = "Add department";
+    this.departmentName = "";
+    this.departmentId = 0;
+  }
+
+  editClick(dep:any){
+    this.modalTitle = "Edit department";
+    this.departmentId = parseInt(dep.DepartmentId);
+    this.departmentName = dep.DepartmentName;
+  }
+
+  createClick(){
+    var val = {
+      DepartmentName:this.departmentName
+    };
+
+    this.http.post(environment.API_URL+'department',val)
+    .subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    })
+  }
+
+  updateClick(){
+    var val = {
+      DepartmentId:this.departmentId,
+      DepartmentName:this.departmentName
+    };
+
+    this.http.put(environment.API_URL+'department',val)
+    .subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    })
+  }
+
+  deleteClick(id:any){
+    if(confirm('Are you sure?'))
+
+    this.http.delete(environment.API_URL+'department/'+id)//send hhtp request
+    .subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    })
   }
 
 }
